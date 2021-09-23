aa19f0e5f882d9d70d829a6a12c7019abc62606f
Grzegorz <grzcichosz@gmail.com>
2021-09-23 09:47:05 +0200

    initial commit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, flash, redirect
 from app.forms import LoginForm
 
 
@@ -13,13 +13,17 @@ def hello_world():
     return 'Hello OK'
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
 
 
-@app.route('/test')
+@app.route('/index')
 def test():
     user = {'username': 'Greg'}
     posts = [
